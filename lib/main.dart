954d40283cc49e9ed6051b2e5c9feffb170ece8a
bnjpcson <pecsonbenjiea@gmail.com>
2024-09-18 22:17:44 +0800

    Added: splash screen
    Installed flutter native splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/welcome.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
+  FlutterNativeSplash.preserve(widgetsBinding: WidgetsFlutterBinding.ensureInitialized());
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
